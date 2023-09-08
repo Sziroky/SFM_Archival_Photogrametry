727603c9a335db61a4b4d16249847e9811c9332d
Sziroky <p.sziroky.us@gmail.com>
2023-09-08 12:40:11 +0200

    Histograms
    Created histograms of every scene to further analyst.

--- Photos_preprocessing/preprocesing_Baranovski.py
+++ Photos_preprocessing/preprocesing_Baranovski.py
@@ -1,6 +1,8 @@
 import cv2
 # Type: pip install opencv-python in terminal to install cv package.
 import os
+import matplotlib
+import matplotlib.pyplot as plt
 
 ''' First thing to do is to crop images from phototeodolite to get rid of this black frame, 
 information described on the frame would be save and extract.
@@ -47,22 +49,41 @@ cut = baranovski_16_08_82[1][153:1946, 135:2683]  # Coordinates Manually Set
 '''Iterate over every Image'''
 cut_images_16_08 = []
 for i in baranovski_16_08_82:
-    cut = i[162:1926, 135:2683]# changing height -- not every image is correctly cropped, greater value should not
+    cut = i[179:1926, 135:2683]  # changing height -- not every image is correctly cropped, greater value should not
     # impact reconstruction which mainly focused on ground forms.
     cut_images_16_08.append(cut)
 
 cut_images_17_08 = []
 for i in baranovski_17_08_82:
-    cut = i[179:1926, 135:2683]# changing height -- not every image is correctly cropped, greater value should not
+    cut = i[179:1926, 135:2683]  # changing height -- not every image is correctly cropped, greater value should not
     # impact reconstruction which mainly focused on ground forms.
     cut_images_17_08.append(cut)
 
 '''We cropped the images, now its time to think about filters that should be applied to get rid of noise and artefacts '''
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('image', 2900, 2050)
-for i in range(6):
-    cv2.imshow("image", cut_images_17_08[i])
-    cv2.waitKey(0)
+# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('image', 2900, 2050)
+# for i in range(6):
+#     cv2.imshow("image", cut_images_17_08[i])
+#     cv2.waitKey(0)
 
 # todo:Apply some filters to normalize images --- According to Ms.Geyman's work.
+
+# Let's generate image histograms to get information about pixel values in every single pic and its name.
+
+n = 23
+for img in cut_images_17_08:
+    histr = cv2.calcHist([img], [0], None, [256], [0, 256])
+    plt.plot(histr)
+    name = f"SV_1982-08-16_{n}"
+    n += 1
+    plt.savefig(fr'D:\SFM_Archival_Photogrametry\Images\Histograms\\{name}')
+
+n = 13
+for img in cut_images_16_08:
+    histr = cv2.calcHist([img], [0], None, [256], [0, 256])
+    plt.plot(histr)
+    name = f"SV_1982-08-16_{n}"
+    n += 1
+    plt.savefig(fr'D:\SFM_Archival_Photogrametry\Images\Histograms\\{name}')
+    
